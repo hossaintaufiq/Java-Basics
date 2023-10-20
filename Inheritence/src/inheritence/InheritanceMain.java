52ab8ff5fa2921ef78862c2e01fa27a64a145cc6
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-10-20 23:18:49 +0600

    Encapsulation

---
+++ Inheritence/src/inheritence/InheritanceMain.java
@@ -0,0 +1,8 @@
+
+package inheritence;
+
+public class InheritanceMain {
+     public static void main(String[] args) {
+         System.out.println(" Hello world");
+    }
+}
---
+++ Inheritence/src/inheritence/Inheritence.java
@@ -0,0 +1,8 @@
+
+package inheritence;
+
+public class Inheritence {
+
+   
+    
+}
